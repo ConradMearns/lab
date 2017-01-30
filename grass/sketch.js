a0d023cc9fdc5e1e69432a322e0bb5e2a8f3d134
Conrad Mearns <mazakiikasai@gmail.com>
2017-01-30 13:42:28 -0800

    Testing with new content

--- grass/sketch.js
+++ grass/sketch.js
@@ -1,16 +1,33 @@
 var grass;
+var tree;
 
 function setup() {
   createCanvas(500, 500, WEBGL);
 
-  grass = loadModel('obj/grass.obj');
+  grass = loadModel('http://conrads.website/p5-sketches/grass/obj/grass.obj');
+  tree = loadModel('http://conrads.website/p5-sketches/grass/obj/tree.obj');
 }
 
 function draw() {
   background(51);
+  ambientLight(150);
 
-  rotateX(frameCount * 0.01);
-  scale(20);
-  model(grass);
+  rotateY(frameCount * 0.01);
+  rotateX(frameCount * 0.002);
+  push();
+    //Grass
+    push();
+    scale(10);
+    ambientMaterial(50, 200, 50);
+    model(grass);
+    pop();
 
+    //Grass
+    push();
+    translate(100, 0, 0);
+    scale(20);
+    ambientMaterial(50, 200, 50);
+    model(tree);
+    pop();
+  pop()
 }
