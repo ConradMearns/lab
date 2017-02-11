9540b295a9840503ea3ca588c4c5ea2a73378407
Conrad Mearns <mazakiikasai@gmail.com>
2017-02-10 16:37:10 -0800

    Update Glitch

--- glitch/sketch.js
+++ glitch/sketch.js
@@ -1,10 +1,13 @@
 var pixoff = 6;
-var image;
+var img;
+
+var test = 128;
 
 function setup(){
   // createCanvas(400, 400, WEBGL);
-  createCanvas(400, 400);
-  image = loadImage('http://conrads.website/lab/glitch/head.png');
+  createCanvas(800, 800);
+  img = loadImage('http://conrads.website/lab/glitch/head.png');
+
 }
 
 function draw(){
@@ -19,17 +22,25 @@ function draw(){
 
   var x = 40;
   var y = 40;
+  var sizex = img.width;
+  var sizey = img.height;
 
-  stroke('red');
-  rect(x-cosoff(rotationX), y-sinoff(rotationX), 90, 90);
+  // stroke('red');
+  // rect(x-cosoff(rotationX), y-sinoff(rotationX), sizex, sizey);
+  tint(255, 0, 0, 128);
+  image(img, x-cosoff(rotationX), y-sinoff(rotationX));
 
-  stroke('cyan');
-  rect(x-cosoff(rotationY), y-sinoff(rotationY), 90, 90);
+  // stroke('cyan');
+  // rect(x-cosoff(rotationY), y-sinoff(rotationY), sizex, sizey);
+  tint(0,255,255, 128);
+  image(img, x+cosoff(rotationY), y+sinoff(rotationY));
 
-  strokeWeight(2);
-  stroke(color(255, 255, 255));
-  fill(color(255, 255, 255));
-  rect(x, y, 90, 90);
+  // strokeWeight(2);
+  // stroke(color(255, 255, 255));
+  // fill(color(255, 255, 255));
+  // rect(x, y, sizex, sizey);
+  tint(255, 255, 255, 128);
+  image(img, x, y);
 }
 
 function cosoff(rotation){
